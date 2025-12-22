e68a1917404d78b02ccbd16fbc7f60c10be16c95
A1iHassan <ali012wkout@gmail.com>
2025-12-22 18:26:19 +0200

    init web server entry

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun! (edited)");
+import express, { json } from 'express'
+import cors from 'cors'
+
+const app = express()
+
+const port = process.env.PORT
+
+app.use(json())
+app.use(cors())
+
+app.listen(port, (err) => {
+    if (err) console.error("Couldn't spin up server\n", err);
+    else console.log("Server up and running!")
+})
